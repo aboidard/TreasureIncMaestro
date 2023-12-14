c7095181be9156a8b712c1cef218526b6bef63a7
arnaud <arnaud.boidard@gmail.com>
2023-12-14 22:35:21 +0100

    add post & put  users

---
+++ src/main/java/org/bogomips/treasureInc/UserConfig.java
@@ -0,0 +1,8 @@
+package org.bogomips.treasureInc;
+
+import io.smallrye.config.ConfigMapping;
+
+@ConfigMapping(prefix = "users")
+public interface UserConfig {
+
+}
---
+++ src/main/java/org/bogomips/treasureInc/UserResource.java
@@ -0,0 +1,41 @@
+package org.bogomips.treasureInc;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+
+@Path("/users")
+public class UserResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> users() {
+        return User.listAll();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{publicKey}")
+    public User users(String publicKey) {
+        return User.findByPublicKey(publicKey);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public User createUser(User user) {
+        user.persist();
+        return user;
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{publicKey}")
+    public User updateUser(String publicKey, User user) {
+        User u = User.findByPublicKey(publicKey);
+        u.setMoney(user.getMoney());
+        u.persist();
+        return u;
+    }
+}
